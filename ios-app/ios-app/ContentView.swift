b5c4980490a26bb59235ff1eceb5606952e7e8ec
Tahsinul Haque Wrudra <rudra483haque@gmail.com>
2026-03-17 01:47:17 +0600

    IOS APP Prep

---
+++ ios-app/ios-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-app
+//
+//  Created by Tahsinul Haque Wrudra on 17/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-app/ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by Tahsinul Haque Wrudra on 17/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
